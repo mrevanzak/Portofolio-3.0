10ab143e7e456c3f3f64a8016c399ee893f0f4bc
mrevanzak <mrevanzak@gmail.com>
2023-05-07 16:21:21 +0700

    feat: color config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,21 +9,9 @@ export default {
         primary: ['Inter', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        primary: {
-          // Customize it on globals.css :root
-          50: 'rgb(var(--tw-color-primary-50) / <alpha-value>)',
-          100: 'rgb(var(--tw-color-primary-100) / <alpha-value>)',
-          200: 'rgb(var(--tw-color-primary-200) / <alpha-value>)',
-          300: 'rgb(var(--tw-color-primary-300) / <alpha-value>)',
-          400: 'rgb(var(--tw-color-primary-400) / <alpha-value>)',
-          500: 'rgb(var(--tw-color-primary-500) / <alpha-value>)',
-          600: 'rgb(var(--tw-color-primary-600) / <alpha-value>)',
-          700: 'rgb(var(--tw-color-primary-700) / <alpha-value>)',
-          800: 'rgb(var(--tw-color-primary-800) / <alpha-value>)',
-          900: 'rgb(var(--tw-color-primary-900) / <alpha-value>)',
-          950: 'rgb(var(--tw-color-primary-950) / <alpha-value>)',
-        },
-        dark: '#222222',
+        primary: 'rgb(var(--tw-color-primary) / <alpha-value>)',
+        secondary: 'rgb(var(--tw-color-secondary) / <alpha-value>)',
+        accent: 'rgb(var(--tw-color-accent) / <alpha-value>)',
       },
       keyframes: {
         flicker: {
