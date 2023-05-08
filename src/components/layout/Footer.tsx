cf6046fae7be1458d446609c6f4510a837fd017d
mrevanzak <mrevanzak@gmail.com>
2023-05-08 16:11:09 +0700

    feat: add header and footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className='layout flex h-24 items-center justify-center'>
+      <p className='text-accent hover:text-secondary font-bold'>Revan</p>
+    </footer>
+  );
+}
