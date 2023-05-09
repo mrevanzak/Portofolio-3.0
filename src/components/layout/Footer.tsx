4fa3b9039657837d5b810cb09dd4ce9ce6fd724d
mrevanzak <mrevanzak@gmail.com>
2023-05-09 17:17:49 +0700

    feat(Footer): edit content

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 export default function Footer() {
+  const year = new Date().getFullYear();
   return (
     <footer className='layout flex h-24 items-center justify-center'>
-      <p className='text-accent hover:text-secondary font-bold'>Revan</p>
+      <p className='text-accent hover:text-secondary font-bold'>
+        © Revanza Kurniawan {year}
+      </p>
     </footer>
   );
 }
